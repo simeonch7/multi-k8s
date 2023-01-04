55a880a561270eff0899c3248f0b84d126e15a97
Simeon Chakarov <simeon.chakarov@smarsh.com>
2023-01-04 11:46:06 +0200

    Final changes

--- parser/script.py
+++ parser/script.py
@@ -1,8 +1,8 @@
-#from google.cloud import storage
 import pandas as pd
 import json
 import time
 import pymongo
+from google.cloud import storage
 
 # Initialise a client
 #storage_client = storage.Client("multi-k8s-369008")
@@ -12,14 +12,24 @@ import pymongo
 #blob = bucket.blob("raw-parquets/userdata1.parquet")
 # Download the file to a destination
 #blob.download_to_filename("userdata1.parquet")
+# The ID of your GCS bucket
+# bucket_name = "your-bucket-name"
+# The ID of your new GCS object
+# blob_name = "storage-object-name"
 
-parquet_path = "./parquet_files/"
-parquet_file = "raw-parquets_userdata1.parquet"
+#storage_client = storage.Client()
+#bucket = storage_client.bucket("data-ims")
+#blob = bucket.blob("userdata1.parquet")
+
+# Mode can be specified as wb/rb for bytes mode.
+# See: https://docs.python.org/3/library/io.html
+#parquet_path = "./parquet_files/"
+#parquet_file = "raw-parquets_userdata1.parquet"
 
 timestamp = str(time.time())
 
 try:
-  print(pd.read_parquet(parquet_path + parquet_file, engine='auto'))
+  print(pd.read_parquet("gs://data-ims/raw-parquets/userdata1.parquet", engine='auto'))
   state = {"filename": parquet_file, "stage": "completed", "time": timestamp}
 except:
   state = {"filename": parquet_file, "stage": "failed", "time": timestamp}
@@ -32,6 +42,6 @@ with open("state" + timestamp + ".json", "w") as outfile:
 
 myclient = pymongo.MongoClient("mongodb://mongodb-cluster-ip-service:27017/")
 mydb = myclient["mydatabase"]
-mycol = mydb["customers"]
+mycol = mydb["run_instances"]
 x = mycol.insert_one(state)
 print(x.inserted_id)
